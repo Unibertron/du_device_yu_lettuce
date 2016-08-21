3539c7b1e15b6f92ace31c87676ad467da13ffe4
DroidThug <saivishal2001@gmail.com>
2016-08-21 15:17:59 +0530

    Jack N Jill went down the hill

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,5 +17,8 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
 include device/yu/lettuce/board/*.mk
 
+# Send Jack N Jill down the DU hill
+ANDROID_COMPILE_WITH_JACK_AND_JILL := false
+
 # inherit from proprietary files
 -include vendor/yu/lettuce/BoardConfigVendor.mk
